d9d004272a649f9f4ed3cc35ef7e5e5d92014a40
fb05 <32671386+fb05@users.noreply.github.com>
2018-03-27 17:46:24 +0200

    changed DB

--- src/main/java/DBAccess/dbInit.sql
+++ src/main/java/DBAccess/dbInit.sql
@@ -4,6 +4,7 @@ CREATE DATABASE  IF NOT EXISTS `useradmin`;
 USE `useradmin`;
 
 DROP TABLE IF EXISTS `Users`;
+DROP TABLE IF EXISTS `order`;
 CREATE TABLE `Users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(90) NOT NULL,
@@ -14,10 +15,11 @@ CREATE TABLE `Users` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `order` (
-`orderID` int(11) NOT NULL AUTO_INCREMENT,
+`orderID`int(11) NOT NULL AUTO_INCREMENT,
 `height`int(11) NOT NULL,
 `length`int(11) NOT NULL,
 `width`int(11) NOT NULL,
+`user_id`int(11) NOT NULL,
 primary key (orderID)
 );
 
